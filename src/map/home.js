fafeedd0c489b07b84363d56e1ef66d61796853b
Ties <tycianol@gmail.com>
2019-05-15 23:11:22 +0200

    GeoJsonLayers

--- src/map/home.js
+++ src/map/home.js
@@ -1,7 +1,8 @@
 require([
     "esri/Map",
-    "esri/views/MapView"
-  ], function(Map, MapView) {
+    "esri/views/MapView",
+    "esri/layers/GeoJSONLayer",
+  ], function(Map, MapView, GeoJSONLayer) {
 
   var map = new Map({
     basemap: "topo-vector"
@@ -13,4 +14,14 @@ require([
     center: [4.895168, 52.370216],
     zoom: 12
   });
+
+  var geoJSONLayer = new GeoJSONLayer({
+    url: "https://map.data.amsterdam.nl/maps/hoofdroutes?service=WFS&request=GetFeature&version=2.0.0&typenames=vrachtroutes&outputformat=geojson&srsname=EPSG:4326",
+    copyright: "Vrachtroute Stadsdeel Centrum >7.5 ton",
+    
+ });
+
+
+map.add(geoJSONLayer);
+
 });

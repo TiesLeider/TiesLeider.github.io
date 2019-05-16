f34f51d66557749ece6b84a617fcad69d3c6fff0
nicovdbrink <nicovdbrink@hotmail.com>
2019-05-16 09:45:19 +0200

    sceneView added

--- src/map/home.js
+++ src/map/home.js
@@ -1,27 +1,29 @@
 require([
     "esri/Map",
-    "esri/views/MapView",
+    "esri/views/SceneView",
     "esri/layers/GeoJSONLayer",
-  ], function(Map, MapView, GeoJSONLayer) {
+  ], function(Map, SceneView, GeoJSONLayer) {
 
   var map = new Map({
-    basemap: "topo-vector"
-  });
-
-  var view = new MapView({
-    container: "viewDiv",
-    map: map,
-    center: [4.895168, 52.370216],
-    zoom: 12
+    basemap: "streets",
+    ground: "world-elevation"
   });
 
   var geoJSONLayer = new GeoJSONLayer({
     url: "https://map.data.amsterdam.nl/maps/hoofdroutes?service=WFS&request=GetFeature&version=2.0.0&typenames=vrachtroutes&outputformat=geojson&srsname=EPSG:4326",
     copyright: "Vrachtroute Stadsdeel Centrum >7.5 ton",
-    
  });
 
+  map.add(geoJSONLayer);
 
-map.add(geoJSONLayer);
+  var view = new SceneView({
+    container: "viewDiv",
+    map: map,
+  });
 
+  view.when(function() {
+    geoJSONLayer.when(function() {
+      view.goTo(geoJSONLayer.fullExtent);
+    });
+  });
 });
